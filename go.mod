ada15e7d38b168bd0afe57bb2017e218d39812ba
Sophie Geyer <sophielizg0@gmail.com>
2023-07-29 17:57:21 -0700

    add json and xml selectors

--- go.mod
+++ go.mod
@@ -4,11 +4,13 @@ go 1.19
 
 require (
 	github.com/antchfx/htmlquery v1.3.0
-	golang.org/x/net v0.5.0
+	github.com/antchfx/jsonquery v1.3.3
+	github.com/antchfx/xmlquery v1.3.17
+	golang.org/x/net v0.7.0
 )
 
 require (
-	github.com/antchfx/xpath v1.2.3 // indirect
+	github.com/antchfx/xpath v1.2.4 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 )
--- request/selectors.go
+++ request/selectors.go
@@ -2,17 +2,19 @@ package request
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
 
 	"golang.org/x/net/html"
 
 	"github.com/antchfx/htmlquery"
+	"github.com/antchfx/jsonquery"
+	"github.com/antchfx/xmlquery"
 )
 
 type SelectorsResult map[string][]string
 
 func applyHtmlSelectors(resp *http.Response, selectors []*Selector) (SelectorsResult, error) {
-	defer resp.Body.Close()
 	doc, err := htmlquery.Parse(resp.Body)
 	if err != nil {
 		return nil, err
@@ -47,10 +49,70 @@ func applyHtmlSelectors(resp *http.Response, selectors []*Selector) (SelectorsRe
 	return results, nil
 }
 
+func applyJsonSelectors(resp *http.Response, selectors []*Selector) (SelectorsResult, error) {
+	doc, err := jsonquery.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	results := SelectorsResult{}
+	for _, selector := range selectors {
+		if selector.PageType != Json {
+			continue
+		}
+
+		nodes, err := jsonquery.QueryAll(doc, selector.Xpath)
+		if err != nil {
+			return nil, err
+		}
+
+		selectorResults := make([]string, len(nodes))
+		for i, node := range nodes {
+			selectorResults[i] = fmt.Sprintf("%s", node.Value())
+		}
+
+		results[selector.Name] = selectorResults
+	}
+
+	return results, nil
+}
+
+func applyXmlSelectors(resp *http.Response, selectors []*Selector) (SelectorsResult, error) {
+	doc, err := xmlquery.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	results := SelectorsResult{}
+	for _, selector := range selectors {
+		if selector.PageType != Json {
+			continue
+		}
+
+		nodes, err := xmlquery.QueryAll(doc, selector.Xpath)
+		if err != nil {
+			return nil, err
+		}
+
+		selectorResults := make([]string, len(nodes))
+		for i, node := range nodes {
+			selectorResults[i] = node.InnerText()
+		}
+
+		results[selector.Name] = selectorResults
+	}
+
+	return results, nil
+}
+
 func applySelectors(resp *http.Response, selectors []*Selector) (SelectorsResult, error) {
 	switch resp.Header.Get("Content-Type") {
 	case "text/html":
 		return applyHtmlSelectors(resp, selectors)
+	case "application/json":
+		return applyJsonSelectors(resp, selectors)
+	case "text/xml", "application/xml":
+		return applyXmlSelectors(resp, selectors)
 	default:
 		return nil, nil
 	}
